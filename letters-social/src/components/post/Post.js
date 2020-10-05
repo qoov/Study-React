2ca6d14016ecc9675666af5598cf6521e2c06e43
winterguard <aliasbonfire@gmail.com>
2020-10-05 09:58:48 +0900

    구성이 달라 노출되지 않음

--- letters-social/src/components/post/Post.js
+++ letters-social/src/components/post/Post.js
@@ -1,15 +1,17 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Content from './Content';
-import Image from './Image';
-import Link from './Link';
-import PostActionSection from './PostActionSection';
-import Comments from '../comment/Comments';
-import DisplayMap from '../map/DisplayMap';
-import UserHeader from '../post/UserHeader';
+import * as API from "../../shared/http";
+import Content from "./Content";
+import Image from "./Image";
+import Link from "./Link";
+import PostActionSection from "./PostActionSection";
+import Comments from "../comment/Comments";
+import Loader from "../Loader";
+import DisplayMap from "../map/DisplayMap";
+import UserHeader from "../post/UserHeader";
 
-import RouterLink from '../router/Link';
+import RouterLink from "../router/Link";
 
 /**
  * Displays a post
@@ -17,27 +19,67 @@ import RouterLink from '../router/Link';
  * @param       {props} props
  * @constructor
  */
-function Post(props) {
-    const { post } = props;
-    return post ? (
-        <div className="post">
-            <RouterLink to={`/posts/${post.id}`}>
-                <span>
-                    <UserHeader date={post.date} user={post.user} />
-                    <Content post={post} />
-                    <Image post={post} />
-                    <Link link={post.link} />
-                </span>
-            </RouterLink>
-            {post.location && <DisplayMap location={post.location} />}
-            <PostActionSection postId={post.id} />
-            <Comments postId={post.id} />
-        </div>
-    ) : null;
+export class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: null,
+      comments: [],
+      showComments: false,
+      user: this.props.user,
+    };
+
+    this.loadPost = this.loadPost.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadPost(this.props.id);
+  }
+
+  loadPost(id) {
+    API.fetchPost(id)
+      .then((response) => response.json())
+      .then((post) => this.setState(() => ({ post })));
+  }
+
+  render() {
+    if (!this.state.post) {
+      return <Loader />;
+    }
+
+    return (
+      <div className="post">
+        <UserHeader date={this.state.post.date} />
+        <Content post={this.state.post} />
+        <Image post={this.state.post} />
+        <Link link={this.state.post.link} />
+        <PostActionSection
+          likes={this.state.post.likes}
+          showComments={this.state.showComments}
+        />
+        <Comments
+          comments={this.state.comments}
+          show={this.state.showComments}
+          post={this.state.post}
+          user={this.state.user}
+        />
+      </div>
+    );
+  }
 }
 
 Post.propTypes = {
-    post: PropTypes.object.isRequired
+  post: PropTypes.shape({
+    comments: PropTypes.array,
+    content: PropTypes.string,
+    date: PropTypes.number,
+    id: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    links: PropTypes.array,
+    location: PropTypes.object,
+    user: PropTypes.object,
+    userId: PropTypes.string,
+  }),
 };
 
 export default Post;
