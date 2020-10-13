8d2be5d219ed66b117fab96127aa61568ee52625
winterguard <aliasbonfire@gmail.com>
2020-10-14 07:52:20 +0900

    HomePage

---
+++ nextjs-blog/_posts/style-guide-101.md
@@ -0,0 +1,5 @@
+---
+title:  "Style Guide"
+---
+**Hello world**, this is my first Next.js blog post and it is written in Markdown.
+I hope you like it!
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,3 +1,38 @@
-export default function Blog() {
-  return <div>Welcome to the Next.js Blog</div>
+import DefaultLayout from '@layouts/default'
+import Link from 'next/link'
+import { getConfig, getAllPosts } from '@api'
+
+export default function Blog(props) {
+  return (
+    <DefaultLayout title={props.title} description={props.description}>
+      <p>List of Posts:</p>
+      <ul>
+        {props.posts.map((post, idx) => {
+          return (
+            <li key={idx}>
+              <Link href={`/posts/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </DefaultLayout>
+  )
+}
+
+export async function getStaticProps() {
+  const config = await getConfig();
+  const allPosts = await getAllPosts();
+
+  console.log(config);
+  console.log(allPosts);
+
+  return {
+    props: {
+      posts: allPosts,
+      title: config.title,
+      description: config.description
+    }
+  }
 }
